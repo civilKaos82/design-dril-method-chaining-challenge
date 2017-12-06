132e5c97ec786c34e72a476c04ae2c191207c0ef
Michael Glaser <m.glaser82@gmail.com>
2017-12-06 10:08:38 -0800

    Refactored the squared primes method for clarity.

--- method_chaining.rb
+++ method_chaining.rb
@@ -10,5 +10,16 @@ end
 
 # This is convoluted. Refactor for clarity.
 def squared_primes(array)
-  array.find_all{|x| (2..x-1).select(){|i| x % i == 0}.count == 0 && x > 1}.map{|p| p*p}
+  #Rewritten code:
+  prime_array = []
+
+  array.each do |array_num|
+    if (2..array_num-1).select{|potential_divisor| array_num % potential_divisor == 0}.count == 0 && array_num > 1
+      prime_array << array_num
+    end
+  end
+
+  squared_primes_array = prime_array.map {|prime_num| prime_num**2}
+  #Original Code:
+  # array.find_all{|x| (2..x-1).select{|i| x % i == 0}.count == 0 && x > 1}.map{|p| p*p}
 end
